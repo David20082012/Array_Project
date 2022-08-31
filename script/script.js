9eeee9be9cd4748d8aff89cf9e6c360f442ceb2c
David20082012 <david.wageeh@gmail.com>
2022-08-31 20:33:40 +0200

    comment for loop & creat While Loop

--- script/script.js
+++ script/script.js
@@ -6,7 +6,17 @@
 // Function
 // Array
 
-for(var i=1 ; i<=100 ; i++ ){
+
+
+// for(var i=1 ; i<=100 ; i++ ){
+//     console.log(i);
+// }
+
+
+
+var i = 1;
+while(i<=100){
     console.log(i);
+    i++;
 }
 
