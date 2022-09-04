f97387d76b0d6cfae2b2688b7932d0fade790f1e
David20082012 <david.wageeh@gmail.com>
2022-09-04 20:30:49 +0200

    get element by class

--- script/session_three.js
+++ script/session_three.js
@@ -1,3 +1,7 @@
 // DOM - BOM
 
-document.getElementById("demo").innerHTML="Hello"
+// document.getElementById("demo").innerHTML="Hello"
+
+var array = document.getElementsByClassName("test"); //=> HTML Collection as an array
+
+console.log(array[0]); // we have to specifiy the index of the item or make a loop to go through all of the items in the array
